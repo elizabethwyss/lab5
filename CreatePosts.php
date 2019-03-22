c075a3e6a5de1c9bd15b23fbc4c28e222cdd1575
BenjaminWyss <b040w377@ku.edu>
2019-03-22 10:04:21 -0500

    Finish php page for creating new posts.

--- CreatePosts.php
+++ CreatePosts.php
@@ -2,6 +2,7 @@
   $username = $_POST["username"];
   $content = $_POST["content"];
   $mysqli = new mysqli("mysql.eecs.ku.edu", "b040w377", "Uefai3Ai", "b040w377");
+  $userFound = false;
 
 /* check connection */
 if ($mysqli->connect_errno)
@@ -15,13 +16,37 @@ $query = "SELECT user_id FROM Users WHERE user_id='" . $username . "'";
 if ($result = $mysqli->query($query))
 {
     /* fetch associative array */
-    while ($row = $result->fetch_assoc())
+    if($row = $result->fetch_assoc())
     {
-        
+      $userFound = true;
+    }
+    else
+    {
+      printf("Error: user " . $username . " not found.");
     }
 
     /* free result set */
     $result->free();
+}
+
+if ($userFound)
+{
+  if ($content == "")
+  {
+    printf("Error: cannot create a blank post.");
+  }
+  else
+  {
+    $query = "INSERT INTO Posts (content, author_id) VALUES ('" . $content . "', '" . $username . "')";
+
+    if ($result = $mysqli->query($query))
+    {
+          printf("Post successfully created!");
+
+        /* free result set */
+        $result->free();
+    }
+  }
 }
 
 /* close connection */
