59cb7ce3a6034acaa4b4b39e4a522fcafe1a322b
BenjaminWyss <b040w377@ku.edu>
2019-03-22 10:39:00 -0500

    Create php page for viewing user posts.

---
+++ ViewUserPosts.php
@@ -0,0 +1,31 @@
+<?php
+  $username = $_POST["username"];
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "b040w377", "Uefai3Ai", "b040w377");
+
+  /* check connection */
+  if ($mysqli->connect_errno)
+  {
+      printf("Connect failed: %s\n", $mysqli->connect_error);
+      exit();
+  }
+
+  echo "<h1>List of all Posts Written by " . $username . "</h1><hr>";
+  echo "<table><tr><th>Posted Content:</th></tr>";
+
+ $query = "SELECT * FROM Posts WHERE author_id='" . $username . "'";
+
+ if ($result = $mysqli->query($query))
+ {
+     /* fetch associative array */
+     while ($row = $result->fetch_assoc())
+     {
+       echo "<tr><td>" . $row["content"] . "</td></tr>";
+     }
+
+     /* free result set */
+     $result->free();
+ }
+
+  echo "</table>";
+
+?>
